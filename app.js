4b6caededde1e79e4b37259f270be32dc63e8ee7
Joshua Lee <joshualee@Joshuas-MacBook-Air.local>
2022-08-27 02:47:35 -0400

    final responsiveness changes added, mongo atlas configured

--- app.js
+++ app.js
@@ -8,12 +8,14 @@ const path = require("path");
 const ejsMate = require("ejs-mate");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const mongoSanitize = require("express-mongo-sanitize");
 const methodOverride = require("method-override");
 const session = require("express-session");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
 const flash = require("connect-flash");
 const ExpressError = require("./utils/ExpressError");
+const helmet = require("helmet");
 
 const User = require("./models/user");
 
@@ -21,8 +23,11 @@ const User = require("./models/user");
 const gameRoutes = require("./routes/game");
 const userRoutes = require("./routes/user");
 
+const MongoDBStore = require("connect-mongo");
 //mongo connection
-mongoose.connect("mongodb://localhost:27017/youjeopardy", {
+const DbUrl = process.env.DB_URL || "mongodb://localhost:27017/youjeopardy";
+console.log(DbUrl);
+mongoose.connect(DbUrl, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
@@ -44,22 +49,54 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, "public")));
 app.use(methodOverride("_method"));
+app.use(mongoSanitize());
 
 const secret = process.env.SESSION_SECRET || "averygoodsecret";
 
+const mongoStoreOptions = { mongoUrl: DbUrl, touchAfter: 24 * 60 * 60 };
+
 const sessionConfig = {
   secret,
-  name: "session",
+  store: MongoDBStore.create(mongoStoreOptions),
+  name: "youJeopSession",
   resave: false,
   saveUninitialized: true,
   cookie: {
     httpOnly: true,
+    // secure: true,
     expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
     maxAge: 1000 * 60 * 60 * 24 * 7,
   },
 };
 app.use(session(sessionConfig));
 app.use(flash());
+app.use(helmet());
+const scriptSrcUrls = ["https://cdn.jsdelivr.net", "https://code.jquery.com"];
+const styleSrcUrls = [
+  "https://cdn.jsdelivr.net",
+  "https://fonts.googleapis.com",
+];
+const connectSrcUrls = [];
+const fontSrcUrls = [
+  "https://fonts.gstatic.com",
+  "https://fonts.googleapis.com",
+];
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: [],
+      connectSrc: ["'self'", ...connectSrcUrls],
+      scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+      styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+      workerSrc: ["'self'", "blob:"],
+      childSrc: ["blob:"],
+      objectSrc: [],
+      imgSrc: ["'self'", "blob:", "data:"],
+      fontSrc: ["'self'", ...fontSrcUrls],
+    },
+  })
+);
+
 app.use(passport.initialize());
 app.use(passport.session());
 
--- schema.js
+++ schema.js
@@ -1,24 +1,46 @@
-const Joi = require("joi");
+const Basejoi = require("joi");
+const sanitizeHtml = require("sanitize-html");
+
+const extension = (joi) => ({
+  type: "string",
+  base: joi.string(),
+  messages: { "string.escapeHTML": "{{#label}} must not include HTML!" },
+  rules: {
+    escapeHTML: {
+      validate(value, helpers) {
+        const clean = sanitizeHtml(value, {
+          allowedTags: [],
+          allowedAttributes: {},
+        });
+        if (clean !== value)
+          return helpers.error("string.escapeHTML", { value });
+        return value;
+      },
+    },
+  },
+});
+
+const Joi = Basejoi.extend(extension);
 
 const userSchema = Joi.object({
-  username: Joi.string().min(1).max(25).required(),
-  password: Joi.string().min(1).required(),
+  username: Joi.string().min(1).max(25).required().escapeHTML(),
+  password: Joi.string().min(1).required().escapeHTML(),
 }).required();
 
 const questionSchema = Joi.object({
-  body: Joi.string().min(1).max(250).required(),
-  answer: Joi.string().min(1).max(250).required(),
+  body: Joi.string().min(1).max(250).required().escapeHTML(),
+  answer: Joi.string().min(1).max(250).required().escapeHTML(),
   points: Joi.number().valid(200, 400, 600, 800, 1000).required(),
 });
 
 const categorySchema = Joi.object({
-  name: Joi.string().min(1).max(50).required(),
+  name: Joi.string().min(1).max(50).required().escapeHTML(),
   questions: Joi.array().min(1).max(5).items(questionSchema),
 });
 
 const gameSchema = Joi.object({
-  title: Joi.string().min(1).max(35).required(),
-  access: Joi.string().valid("public", "private").required(),
+  title: Joi.string().min(1).max(35).required().escapeHTML(),
+  access: Joi.string().valid("public", "private").required().escapeHTML(),
   categories: Joi.array().min(1).max(6).items(categorySchema),
 }).required();
 
